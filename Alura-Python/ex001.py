bea102800077ade5b0704656d419de4b3b507455
Marcello Dias <59810968+marcellods99@users.noreply.github.com>
2026-01-12 20:54:20 -0300

    commit

--- Alura-Python/ex001.py
+++ Alura-Python/ex001.py
@@ -1,3 +1,5 @@
+#Julia é professora e precisa de um programa para ajudar seus alunos a calcularem suas idades com base no ano de nascimento. Sua tarefa é criar uma função que receba o ano de nascimento e o ano atual e retorne à idade correspondente.
+
 def calcular_idade(ano_de_nascimento, ano_atual):
   return ano_atual - ano_de_nascimento   
   
