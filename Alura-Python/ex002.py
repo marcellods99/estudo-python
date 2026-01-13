f1a952abb8421a2c2acb2af7b9f35725af38c878
Marcello Dias <59810968+marcellods99@users.noreply.github.com>
2026-01-13 20:39:15 -0300

    commit

--- Alura-Python/ex002.py
+++ Alura-Python/ex002.py
@@ -2,3 +2,8 @@
 
 Ajude Sara criando uma função que receba uma palavra e exiba a quantidade de caracteres."""
 
+def contar_caracteres(palavra): 
+    return len(palavra) 
+ 
+texto = input("Digite uma palavra: ") 
+print(f"Essa palavra tem {contar_caracteres(texto)} caracteres.") 
