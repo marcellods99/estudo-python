7ae157df49d3a0b07d938c9fc14c98c140908167
Marcello Dias <59810968+marcellods99@users.noreply.github.com>
2026-01-13 21:05:44 -0300

    commit

--- Alura-Python/ex003.py
+++ Alura-Python/ex003.py
@@ -1,7 +1,4 @@
 """
-Faça como eu fiz: saudação personalizada
- Próxima Atividade
-
 Beatriz está desenvolvendo um sistema de atendimento para um site de serviços. Ela deseja criar um programa que exiba uma saudação personalizada dependendo da hora do dia que o usuário acessa a plataforma. O sistema deverá ter a seguinte regra:
 
 Se for antes das 12h, exibir "Bom dia";
