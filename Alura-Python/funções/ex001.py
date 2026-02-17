c440870fe22884a9f45656761e8b22b2ddb817a9
marcellods99 <dempseycontato@gmail.com>
2026-02-17 19:17:45 -0300

    teste

--- Alura-Python/funções/ex001.py
+++ Alura-Python/funções/ex001.py
@@ -7,5 +7,3 @@ ano_de_nascimento = int(input('Digite o ano do seu nascimento: '))
 ano_atual = int(input('Digite o ano atual: '))
 idade_atual = ano_atual - ano_de_nascimento
 print(f'Você nasceu no ano {ano_de_nascimento} e o ano atual é {ano_atual}!!\nLogo você tem: {idade_atual} anos!')
-
-#blablabla
