2a832deb8b0f46169c924df44e18b876f7c7fbde
marcellods99 <dempseycontato@gmail.com>
2026-02-17 19:32:15 -0300

    exercicio 2 concluído

--- Alura-Python/introdução/ex002.py
+++ Alura-Python/introdução/ex002.py
@@ -1,1 +1,5 @@
 #2 - Imprima a frase: Meu nome é {nome} e tenho {idade} anos em que nome e idade precisam ser valores armazenados em variáveis.
+
+nome = str(input('Digite o seu nome: '))
+idade = int(input('Quantos anos você tem? '))
+print(f'Meu nome é {nome} e eu tenho {idade} anos! ')
