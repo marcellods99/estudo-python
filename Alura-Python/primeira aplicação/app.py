a50bace546a7da16854191f067c76647e325e911
marcellods99 <dempseycontato@gmail.com>
2026-02-24 13:50:22 -0300

    modificação mudando de str para int

--- Alura-Python/primeira aplicação/app.py
+++ Alura-Python/primeira aplicação/app.py
@@ -12,13 +12,14 @@ print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+opcao_escolhida = int(input('Escolha uma opção: '))
+# opcao_escolhida = int(opcao_escolhida)
 
-if opcao_escolhida == "1":
+if opcao_escolhida == 1:
     print('Cadastrar restaurante')
-elif opcao_escolhida == "2":
+elif opcao_escolhida == 2:
     print('Listar restaurante')
-elif opcao_escolhida == "3":
+elif opcao_escolhida == 3:
     print('Ativar restaurante')
 else:
     print('Encerrando o programa')
