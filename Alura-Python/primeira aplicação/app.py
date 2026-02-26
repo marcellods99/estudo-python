12532f5b16528297e0d3f5707a006ea5065f42e9
marcellods99 <dempseycontato@gmail.com>
2026-02-26 17:00:25 -0300

    atribuindo funções

--- Alura-Python/primeira aplicação/app.py
+++ Alura-Python/primeira aplicação/app.py
@@ -17,20 +17,23 @@ def exibir_opcoes():
     print('3. Ativar restaurante')
     print('4. Sair\n')
 
-opcao_escolhida = int(input('Escolha uma opção: '))
-# opcao_escolhida = int(opcao_escolhida)
 def finalizar_app():
     os.system('cls')
     print('Finalizando o app\n')
 
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-elif opcao_escolhida == 2:
-    print('Listar restaurante')
-elif opcao_escolhida == 3:
-    print('Ativar restaurante')
-else:
-    finalizar_app()
+
+opcao_escolhida = int(input('Escolha uma opção: '))
+# opcao_escolhida = int(opcao_escolhida)
+
+def escolher_opcao():
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurante')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
 
 def main():
     exibir_nome_do_programa()
