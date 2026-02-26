055698ade4e05e6ea8454b8522fcd1b95dedd67a
marcellods99 <dempseycontato@gmail.com>
2026-02-26 17:16:15 -0300

    deixando o código em ordem

--- Alura-Python/primeira aplicação/app.py
+++ Alura-Python/primeira aplicação/app.py
@@ -21,23 +21,23 @@ def finalizar_app():
     os.system('cls')
     print('Finalizando o app\n')
 
-
-opcao_escolhida = int(input('Escolha uma opção: '))
-# opcao_escolhida = int(opcao_escolhida)
-
 def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    # opcao_escolhida = int(opcao_escolhida)
+    
     if opcao_escolhida == 1:
         print('Cadastrar restaurante')
     elif opcao_escolhida == 2:
-        print('Listar restaurante')
+        print('Listar restaurantes')
     elif opcao_escolhida == 3:
-        print('Ativar restaurante')
+        print('Ativar restaurantes')
     else:
         finalizar_app()
 
 def main():
     exibir_nome_do_programa()
     exibir_opcoes()
+    escolher_opcao()
 
 if __name__ == '__main__':
     main()
