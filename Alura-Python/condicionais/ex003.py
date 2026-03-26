28d235bbdce0cb6c7d6d39c0ad2414bcdb258538
marcellods99 <dempseycontato@gmail.com>
2026-03-26 20:58:19 -0300

    novo exercicio

--- Alura-Python/condicionais/ex003.py
+++ Alura-Python/condicionais/ex003.py
@@ -1,1 +1,12 @@
 #3 - Solicite um nome de usuário e uma senha e use uma estrutura if else para verificar se o nome de usuário e a senha fornecidos correspondem aos valores esperados determinados por você.
+
+usuario = str(input('Nome do usuário: '))
+senha = input('Senha do usuário: ')
+
+nome_usuario = "noitepraia"
+senha_usuario = "praiabonita"
+
+if usuario == nome_usuario and senha == senha_usuario:
+    print('Usuário correto')
+else:
+    print("Dados incorretos")
