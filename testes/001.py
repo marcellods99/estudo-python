26ab8351d288452a1c4b5d41b6f4060ea36717c7
marcellods99 <dempseycontato@gmail.com>
2026-04-15 18:13:29 -0300

    add test

---
+++ testes/001.py
@@ -0,0 +1,9 @@
+nome_1 = 'Mariana Rodrigues'
+nome_2 = 'Marcelo Nogueira'
+
+lista = 'José da Silva, Maria Oliveira, Pedro Martins, Ana Souza, Carlos Rodrigues, Juliana Santos, Bruno Gomes, Beatriz Costa, Felipe Almeida, Mariana Fernandes, João Pinto, Luísa Nascimento, Gabriel Souza, Manuela Santos, Thiago Oliveira, Sofia Ferreira, Rafael Albuquerque, Isabella Gomes, Bruno Costa, Maria Martins, Rafaela Souza, Matheus Fernandes, Luísa Almeida, Beatriz Pinto, Mariana Rodrigues, Gabriel Nascimento, João Ferreira, Maria Albuquerque, Felipe Oliveira'
+
+if nome_1 in lista:
+  print(f'{nome_2} está na lista')
+else:
+  print(f'{nome_2} não está na lista')
