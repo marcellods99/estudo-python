5cc315d9aa70a787d92c3c289cef5e1d5a2fed24
marcellods99 <dempseycontato@gmail.com>
2026-04-28 20:06:12 -0300

    add ex

---
+++ Alura Python/Exercicios/Aula 3/Aquecendo na programação/ex010.py
@@ -0,0 +1,2 @@
+#10) Um programa deve ser escrito para ler dois números e, em seguida, perguntar à pessoa usuária qual operação ele deseja realizar. O resultado da operação deve incluir informações sobre o número - se é par ou ímpar, positivo ou negativo e inteiro ou decimal.
+
