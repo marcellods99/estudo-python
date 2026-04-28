01d4dd18d8532fc9751c68b350cbc1c7a2690dd3
marcellods99 <dempseycontato@gmail.com>
2026-04-28 20:09:44 -0300

    a

--- Alura Python/Exercicios/Aula 3/Aquecendo na programação/ex010.py
+++ Alura Python/Exercicios/Aula 3/Aquecendo na programação/ex010.py
@@ -1,2 +1,5 @@
 #10) Um programa deve ser escrito para ler dois números e, em seguida, perguntar à pessoa usuária qual operação ele deseja realizar. O resultado da operação deve incluir informações sobre o número - se é par ou ímpar, positivo ou negativo e inteiro ou decimal.
 
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+
