e52d3093bced86dfa1ccf97b6b9af7295e2e0535
marcellods99 <dempseycontato@gmail.com>
2026-05-17 09:57:02 -0300

    adicionando exercício

--- Alura Python/Exercicios/Aula 4/ex008.py
+++ Alura Python/Exercicios/Aula 4/ex008.py
@@ -1,4 +1,4 @@
-##8) Vamos entender a distribuição de idades de pensionistas de uma empresa de previdência. Escreva um programa que leia as idades de uma quantidade não informada de clientes e mostre a distribuição em intervalos de [0-25], [26-50], [51-75] e [76-100]. Encerre a entrada de dados com um número negativo.
+#8) Vamos entender a distribuição de idades de pensionistas de uma empresa de previdência. Escreva um programa que leia as idades de uma quantidade não informada de clientes e mostre a distribuição em intervalos de [0-25], [26-50], [51-75] e [76-100]. Encerre a entrada de dados com um número negativo.
 
 faixa1 = [0] # 0-25
 faixa2 = [0] # 26-50
