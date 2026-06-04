f4dcfc373b73dcaab0977dd29193f94aa79113d1
marcellods99 <marcellods99@users.noreply.github.com>
2026-06-04 17:16:59 -0300

    add ex

---
+++ Alura Python/Exercicios/Prática/ex004.py
@@ -0,0 +1,6 @@
+#Peça uma palavra ao usuário e mostre cada letra separadamente.
+
+palavra = str(input('Digite uma palavra: '))
+
+for palavra in range():
+    print(palavra)
---
+++ Alura Python/Exercicios/Prática/ex005.py
@@ -0,0 +1,2 @@
+lista = ['Fabricio Daniel', '9.5', '9.0', '8.0', True]
+print(lista[-5])
