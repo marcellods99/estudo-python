a7c150e3cab2db005b6b4f46a9500c5d05a39a97
marcellods99 <marcellods99@users.noreply.github.com>
2026-06-09 17:04:14 -0300

    novos testes

---
+++ Alura Python/Exercicios/Prática/ex008.py
@@ -0,0 +1,10 @@
+raca_caes = ['Labrador Retriever',
+
+             'Bulldog Francês',
+
+             'Pastor Alemão',
+
+             'Poodle']
+
+raca_caes.insert(1, 'Golden Retriever')
+print(raca_caes)
