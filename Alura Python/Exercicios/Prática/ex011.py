82ff530f33f32f704514e02dd9878f07813a2da3
marcellods99 <dempseycontato@gmail.com>
2026-06-23 21:00:33 -0300

    continuação ex

--- Alura Python/Exercicios/Prática/ex011.py
+++ Alura Python/Exercicios/Prática/ex011.py
@@ -1,1 +1,4 @@
 #2) Com os mesmos dados da questão anterior, defina quantas compras foram realizadas acima de 3000 reais e calcule a porcentagem quanto ao total de compras.
+
+empresa = [2172.54 , 3701.35 , 3518.08 , 3426.61 , 3249.38 , 2840.82 , 2840.82 , 3891.45 , 3075.26 , 2317.64 , 3219.08]
+
