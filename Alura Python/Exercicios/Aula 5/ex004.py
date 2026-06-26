bc1385d2d9204330b3f153eaace68180027b93e5
marcellods99 <dempseycontato@gmail.com>
2026-06-25 21:04:12 -0300

    adicionando exercício

--- Alura Python/Exercicios/Aula 5/ex004.py
+++ Alura Python/Exercicios/Aula 5/ex004.py
@@ -1,1 +1,8 @@
-#4) Colete novamente 5 inteiros e imprima a lista em ordem inversa à enviada.
+lista = []
+
+for c in range (0 , 5):
+    numero = int(input('Digite um número inteiro: '))
+    lista.append(numero)
+
+lista.reverse()
+print(lista)
