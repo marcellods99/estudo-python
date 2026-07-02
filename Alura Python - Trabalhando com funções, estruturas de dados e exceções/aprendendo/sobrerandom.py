4a85b0dbbe8c32586b9f0e32f03233405abb84f0
marcellods99 <dempseycontato@gmail.com>
2026-07-02 18:28:06 -0300

    sobre random

--- Alura Python - Trabalhando com funções, estruturas de dados e exceções/aprendendo/sobrerandom.py
+++ Alura Python - Trabalhando com funções, estruturas de dados e exceções/aprendendo/sobrerandom.py
@@ -1,3 +1,6 @@
-import matplotlib.pyplot as plt
+from random import choices
 
 estudantes_2 = ["João", "Maria", "José", "Ana"]
+estudantes = choices(estudantes_2) #Usa biblioteca choices para sortear um nome
+
+print(estudantes)
