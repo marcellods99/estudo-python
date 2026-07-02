7305b6530a9d99f3e6840c520d095af6ac75ba3e
marcellods99 <dempseycontato@gmail.com>
2026-07-02 18:31:11 -0300

    testando a redução do import

--- Alura Python - Trabalhando com funções, estruturas de dados e exceções/aprendendo/sobrerandom.py
+++ Alura Python - Trabalhando com funções, estruturas de dados e exceções/aprendendo/sobrerandom.py
@@ -1,6 +1,6 @@
-from random import choices
+from random import choices as ch #AS altera o nome do import, deixando mais legível e menos poluído
 
 estudantes_2 = ["João", "Maria", "José", "Ana"]
-estudantes = choices(estudantes_2) #Usa biblioteca choices para sortear um nome
+estudantes = ch(estudantes_2) #Usa biblioteca choices para sortear um nome
 
 print(estudantes)
