420a8cb13a9c97ce2ca1dd082f6e7cd36c82eade
marcellods99 <dempseycontato@gmail.com>
2026-07-02 18:56:42 -0300

    adicionando comentários

--- Alura Python - Trabalhando com funções, estruturas de dados e exceções/aprendendo/exemplo_1.py
+++ Alura Python - Trabalhando com funções, estruturas de dados e exceções/aprendendo/exemplo_1.py
@@ -3,7 +3,7 @@ from random import randrange, sample
 lista = []
 
 for i in range(0, 20):
-  lista.append(randrange(100))
+  lista.append(randrange(100)) #append adiciona na lista e o randrange pega os números dentro dessa lista
 
 
-print(sample(lista, 5))
+print(sample(lista, 5)) #sample pega 5 números aleatórios da lista
